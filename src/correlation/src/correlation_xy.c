5e27f0ac84e4e8e460a51bd9440ddc2f3592f530
Clive Emary <31550614+gloygum@users.noreply.github.com>
2022-10-08 08:20:57 +0100

    Update correlation_xy.c

--- src/correlation/src/correlation_xy.c
+++ src/correlation/src/correlation_xy.c
@@ -1,7 +1,7 @@
 /*
     Example to investigate correlation between two random variables, one of which
     is linearly dependent on the other, using  Signaloid unvertain lib.
-    Clive Emary 02-10-22
+    Clive Emary 08-10-22
 */
 
 #include <math.h>
@@ -20,51 +20,54 @@ static double correlation(double , double );
 int
 main(int argc, char *	argv[])
 {
-    // x,z are indepdent r.v.
+   	// x,z are indepdent r.v.
 	double	x,z;
 
 	loadInputs(&x, &z);
 
-    // y in a linear combination of x and z
+   	// y in a linear combination of x and z
 	double y = x+z;
+	
+	printf("x and z and independent random vairables. y =(x+z)/2\n);
 
-    // examine distribution of y
+   	// examine distribution of y
+	printf("Distribution of y:\n);
 	printf("y = %.2E \n",y);
-    libUncertainDoublePrint(y);
+	libUncertainDoublePrint(y);
 
-    // CE comment
-    printf("This distribution is correct.\n");
-    printf("\nCorrelation: rho(x,y) = %.2f\n",correlation(x,y));
-    printf("This correlation is not correct. It should be 1/Sqrt[2]\n");
+	// CE comment
+	printf("This distribution is correct.\n");
+	printf("\nCorrelation: rho(x,y) = %.2f\n",correlation(x,y));
+	printf("This correlation is not correct. It should be 1/Sqrt[2]\n");
 
-    // Alternative method without input loader
-    // in case this makes a difference
-    x  = libUncertainDoubleGaussDist(1, 1 );
-    z  = libUncertainDoubleGaussDist(1, 1 );
-    y = x+z;
+	// Alternative method without input loader
+	// in case this makes a difference
+	x  = libUncertainDoubleGaussDist(1, 1 );
+	z  = libUncertainDoubleGaussDist(1, 1 );
+	y = x+z;
 
-    // CE comment
-    printf("\n\n2nd method --- gives the same as before \n");
+	// CE comment
+	printf("\n\n2nd method --- gives the same as before \n");
 	printf("y = %.2E \n",y);
-    libUncertainDoublePrint(y);
-    printf("Correlation rho(x,y) = %.2f\n",correlation(x,y));
+	libUncertainDoublePrint(y);
+	printf("Correlation rho(x,y) = %.2f\n",correlation(x,y));
 
 
 
 
-    // evaluate the pieces by hand
-    printf("\nIf I decompose the correlation function by hand and evaluate the bits individually, I get\n");
-    // numerator = <xy> - <x><y>
-    double numerator = 0.5*libUncertainDoubleNthMoment(x*x,1) + 0.5*libUncertainDoubleNthMoment(x*z,1)
-                - libUncertainDoubleNthMoment(x,1)*0.5*(libUncertainDoubleNthMoment(x,1)+libUncertainDoubleNthMoment(z,1));
+	// evaluate the pieces by hand
+	printf("\nIf I decompose the correlation function by hand and evaluate the bits individually, I get\n");
+	// numerator = <xy> - <x><y>
+	double numerator = 0.5*libUncertainDoubleNthMoment(x*x,1) + 0.5*libUncertainDoubleNthMoment(x*z,1)
+		- libUncertainDoubleNthMoment(x,1)*0.5*(libUncertainDoubleNthMoment(x,1)+libUncertainDoubleNthMoment(z,1));
 
-    // denominator = \sqrt(<x^2>_c <y^2>_c )
-    double denominator = sqrt(
-                                libUncertainDoubleNthMoment(x,2) * 0.25 * (libUncertainDoubleNthMoment(x,2) + libUncertainDoubleNthMoment(z,2))
-                            );
-    //CE comment
-    printf("   rho(x,y) = %.02f\n",numerator/denominator);
-    printf("This is the correct answer....");
+	// denominator = \sqrt(<x^2>_c <y^2>_c )
+	double denominator = sqrt(
+				libUncertainDoubleNthMoment(x,2) * 0.25 * (libUncertainDoubleNthMoment(x,2) + libUncertainDoubleNthMoment(z,2))
+			    );
+	//CE comment
+	printf("   rho(x,y) = %.02f\n",numerator/denominator);
+	printf("This is the correct answer....");
 
 	return 0;
 }
@@ -74,10 +77,8 @@ main(int argc, char *	argv[])
 static void
 loadInputs(double *  x, double *  z)
 {
-
-
-    *x  = libUncertainDoubleGaussDist(1, 1 );
-    *z  = libUncertainDoubleGaussDist(1, 1 );
+	*x  = libUncertainDoubleGaussDist(1, 1 );
+	*z  = libUncertainDoubleGaussDist(1, 1 );
 
 }
 
@@ -92,8 +93,8 @@ loadInputs(double *  x, double *  z)
             with sigma_{X,Y} the standard deviations of the two distributions
 */
 static double correlation(double X, double Y){
-    double rho = ( libUncertainDoubleNthMoment(X*Y,1) -  libUncertainDoubleNthMoment(X,1)*  libUncertainDoubleNthMoment(Y,1))
-                 / sqrt(libUncertainDoubleNthMoment(X,2) *  libUncertainDoubleNthMoment(Y,2));
+	double rho = ( libUncertainDoubleNthMoment(X*Y,1) -  libUncertainDoubleNthMoment(X,1)*  libUncertainDoubleNthMoment(Y,1))
+		 / sqrt(libUncertainDoubleNthMoment(X,2) *  libUncertainDoubleNthMoment(Y,2));
 
-    return rho;
+	return rho;
 }
